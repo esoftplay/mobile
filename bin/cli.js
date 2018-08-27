88dd9d4a59754708ece9bef4323b5fb4388a15ad
Danang Widiantoro <danang@fisip.net>
2018-08-27 16:28:22 +0700

    v0.0.24

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 const { spawn } = require('child_process');
+const fs = require('fs');
 var app = require('../watch')
-var fs = require('fs')
+var path = "./node_modules/esoftplay/cache/";
+var ppid = path+"pid.txt";
 
 function execution()
 {
@@ -14,25 +16,40 @@ function execution()
 	  console.log('stderr: ' + data.toString());
 	});
 }
+function watcher()
+{
+	watchstop()
+	var watch  = spawn('node', ['node_modules/.bin/esoftplay', 'watch'], {stdio: 'ignore', detached: true });
+	const newPID = watch.pid;
+	watch.unref();
+	if (newPID) {
+		if (!fs.existsSync(path)) {
+			fs.mkdirSync(path);
+		}
+		fs.writeFile(ppid, newPID, (err) => {
+		  if (err) throw err;
+		  // console.log('new PID has been cached');
+		});
+	}
+}
+function watchstop() {
+	if (fs.existsSync(ppid)) {
+		var oldPID = fs.readFileSync(ppid, 'utf8').trim();
+		if (oldPID) {
+			spawn("kill", [oldPID], {stdio: 'ignore', detached: true }, function(stat){
+				fs.unlinkSync(ppid);
+			});
+		}
+	}
+}
 
 var paths = ["./assets/", "./modules/", "./templates/"]
 var args = process.argv.slice(2);
 switch(args[0]) {
+	case "test":
 	case "start":
 		execution();
-		// var nd = spawn('node', ['node_modules/.bin/esoftplay', 'watch']);
-		// var nd = spawn('node', ['node_modules/.bin/esoftplay', 'watch']);
-		// nd.stdout.on('data', function(data) {
-		// 	console.log('stdout (' + nd.pid + '): ' + data);
-		// });
-		// console.log("sduhfsdfsd sdf ")
-		break;
-	case "test":
-		// execution();
-		var nd = spawn('node', ['node_modules/.bin/esoftplay', 'watch'], {
-			stdio: 'ignore',
-			detached: true
-		}).unref();
+		watcher();
 		break;
 	case "watch":
 		for (var i = 0; i < paths.length; i++) {
@@ -48,6 +65,7 @@ switch(args[0]) {
 		}
 		break;
 	case "stop":
+		watchstop();
 		for (var i = 0; i < paths.length; i++) {
 			const path = paths[i]
 			app.unwatchTree(path)
--- bin/test.sh
+++ bin/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-DST="$( pwd )/node_modules/esoftplay/bin"
-rm -f $DST/build.js
-ln -s $SRC/build.js $DST/build.js
-rm -f $DST/cli.js
-ln -s $SRC/cli.js $DST/cli.js
-rm -f $DST/router.js
-ln -s $SRC/router.js $DST/router.js
+SRC="$( cd "$( dirname $(dirname "${BASH_SOURCE[0]}") )" >/dev/null && pwd )"
+DST="$( pwd )/node_modules/esoftplay"
+rm -rf $DST/bin
+ln -s $SRC/bin $DST/bin
+rm -rf $DST/modules
+ln -s $SRC/modules $DST/modules
+rm -rf $DST/index.js
+ln -s $SRC/index.js $DST/index.js
+rm -rf $DST/watch.js
+ln -s $SRC/watch.js $DST/watch.js
