519771fdbc0b3f35d70da06e8b73352823c9c0e8
kholil <kholil@fisip.net>
2018-09-10 06:59:45 +0700

    add badge notification

---
+++ modules/content/badge.js
@@ -0,0 +1,38 @@
+
+import React, { Component } from '../../../react';
+import { View } from '../../../react-native/Libraries/react-native/react-native-implementation.js';
+import { Badge, Text } from '../../../native-base'
+import esp from 'esoftplay';
+import { TouchableOpacity } from 'react-native';
+import { connect } from '../../../react-redux';
+const Enotification = esp.mod('content/notification');
+
+
+class Ebadge extends Component {
+
+  static mapStateToProps = (state) => {
+    return {
+      data: state.content_notification.data
+    }
+  }
+
+  componentDidMount() {
+    Enotification.action.content_notification_loadData()
+  }
+
+  render = () => {
+    const { data } = this.props
+    const counter = data.filter((item) => item.status != 2).length
+    if (counter == 0) { return null }
+    return (
+      <View style={{ position: 'absolute', top: 5, right: 5 }} >
+        <TouchableOpacity onPress={() => this.props.onPress()} >
+          <Badge style={{ transform: [{ scale: 0.7 }] }} >
+            <Text>{counter}</Text>
+          </Badge>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+module.exports = connect(Ebadge.mapStateToProps)(Ebadge);
--- modules/content/notification.js
+++ modules/content/notification.js
@@ -1,91 +1,126 @@
 import React, { Component } from '../../../react';
-import { View, Alert, Linking, StatusBar } from '../../../react-native/Libraries/react-native/react-native-implementation.js';
-const Elist = esp.mod('lib/list');
-const { colorPrimary, colorAccent, elevation, STATUSBAR_HEIGHT } = esp.mod('lib/style');
-const Curl = esp.mod('lib/curl');
-import { Text, Button, Icon } from 'native-base';
-const Member = esp.mod('content/member');
+import { TouchableOpacity, View, Alert, Linking, StatusBar } from '../../../react-native/Libraries/react-native/react-native-implementation.js';
+import esp from '../../../esoftplay';
+import { store } from '../../../../App';
+import { connect } from '../../../react-redux'
 import moment from '../../../moment/min/moment-with-locales'
-const utils = esp.mod('lib/utils');
 import update from 'immutability-helper'
-const crypt = esp.mod('lib/crypt');
-const EdbNotif = esp.mod('db/notification');
-const db = new EdbNotif();
-const salt = esp.config('salt');
-import esp from 'esoftplay';
-import { TouchableOpacity } from 'react-native';
-
+import { Text, Button, Icon } from 'native-base';
 class Enotification extends Component {
 
-  state = {
-    data: []
-  }
-
-  fetchData(uri, post, db) {
-    new Curl(uri, post,
-      (res, msg) => {
-        var list = res.list
-        list.map((row) => {
-          db.insertOrUpdate(row)
-        })
-        if (res.next != '') {
-          this.fetchData(res.next, post, db)
+  static reducer = (state = { data: [] }, action) => {
+    switch (action.type) {
+      case 'content_notification_parseData':
+        return {
+          data: action.payload
         }
-        if (list.length > 0) {
-          this.loadData()
+        break
+      case 'content_notification_setRead':
+        var data = state.data
+        var itemData = data.filter((item) => item.id == action.payload)[0]
+        var query = {
+          [data.indexOf(itemData)]: {
+            status: { $set: 2 }
+          }
         }
-        esp.log(res)
-      }, (msg) => {
-        esp.log(msg)
-      }, 1
-    )
-  }
-
-  loadData() {
-    db.getAll((res) => {
-      this.setState({ data: res });
-    })
-  }
-
-  setRead(id) {
-    var data = this.state.data
-    var itemData = data.filter((item) => item.id == id)[0]
-    var query = {
-      [data.indexOf(itemData)]: {
-        status: { $set: 2 }
-      }
+        esp.log(itemData, data.indexOf(itemData), query);
+        return {
+          data: update(data, query)
+        }
+      default:
+        return state
     }
-    this.setState({ data: update(data, query) })
+  }
+
+  static action = {
+    content_notification_loadData() {
+      var uri = 'user/push-notif'
+      try { Enotification.action.content_notification_parseData() } catch (error) { }
+      const EdbNotif = esp.mod('db/notification');
+      const db = new EdbNotif();
+      db.execute('SELECT notif_id FROM notification WHERE 1 ORDER BY notif_id DESC LIMIT 1', (res) => {
+        if (res.rows.length > 0) {
+          uri += '?last_id=' + res.rows._array[0].notif_id
+        }
+        esp.log(res);
+        const crypt = esp.mod('lib/crypt');
+        const salt = esp.config('salt');
+        var post = {
+          user_id: '',
+          secretkey: crypt.encode(salt + '|' + moment().format('YYYY-MM-DD hh:mm:ss'))
+        }
+        const Member = esp.mod('content/member');
+        Member.load((member) => {
+          if (member) post['user_id'] = member.user_id
+          Enotification.action.content_notification_fetchData(uri, post, db);
+        })
+      }, 1)
+    },
+    content_notification_fetchData(uri, post, db) {
+      const Curl = esp.mod('lib/curl');
+      new Curl(uri, post,
+        (res, msg) => {
+          var list = res.list
+          list.map((row) => {
+            db.insertOrUpdate(row)
+          })
+          if (res.next != '') {
+            Enotification.action.content_notification_fetchData(res.next, post, db)
+          }
+          if (list.length > 0) {
+            try { Enotification.action.content_notification_parseData() } catch (error) { }
+          }
+          esp.log(res)
+        }, (msg) => {
+          esp.log(msg)
+        }, 1
+      )
+    },
+    content_notification_parseData() {
+      return store.dispatch((dispatch) => {
+        const EdbNotif = esp.mod('db/notification');
+        const db = new EdbNotif();
+        db.getAll((res) => {
+          dispatch({
+            type: 'content_notification_parseData',
+            payload: res
+          })
+        })
+      })
+    },
+    content_notification_setRead(id) {
+      return store.dispatch({
+        type: 'content_notification_setRead',
+        payload: id
+      })
+    }
+
+  }
+
+  static mapStateToProps = (state) => {
+    return {
+      data: state.content_notification.data
+    }
   }
 
   componentDidMount() {
     moment.locale('id')
-    var uri = 'user/push-notif'
-    this.loadData()
-    db.execute('SELECT notif_id FROM notification WHERE 1 ORDER BY notif_id DESC LIMIT 1', (res) => {
-      if (res.rows.length > 0) {
-        uri += '?last_id=' + res.rows._array[0].notif_id
-      }
-      esp.log(res);
-      var post = {
-        user_id: '',
-        secretkey: crypt.encode(salt + '|' + moment().format('YYYY-MM-DD hh:mm:ss'))
-      }
-      Member.load((member) => {
-        if (member) post['user_id'] = member.user_id
-        this.fetchData(uri, post, db);
-      })
-    }, 1)
+    Enotification.action.content_notification_loadData()
   }
 
   openNotif(data) {
+    const Curl = esp.mod('lib/curl');
+    const crypt = esp.mod('lib/crypt');
+    const salt = esp.config('salt');
     new Curl('user/push-read', {
       notif_id: data.notif_id,
       secretkey: crypt.encode(salt + '|' + moment().format('YYYY-MM-DD hh:mm:ss'))
     }, (res, msg) => {
       esp.log(res)
+      const EdbNotif = esp.mod('db/notification');
+      const db = new EdbNotif();
       db.setRead(data.id)
-      this.setRead(data.id)
+      Enotification.action.content_notification_setRead(data.id)
     }, (msg) => {
       esp.log(msg)
     }, 1)
@@ -119,6 +154,8 @@ class Enotification extends Component {
 
 
   render = () => {
+    const { colorPrimary, colorAccent, elevation, STATUSBAR_HEIGHT } = esp.mod('lib/style');
+    const Elist = esp.mod('lib/list');
     const { goBack } = this.props.navigation
     return (
       <View style={{ flex: 1, backgroundColor: 'white' }}>
@@ -142,7 +179,7 @@ class Enotification extends Component {
             }}>Notifikasi</Text>
         </View>
         <Elist
-          data={this.state.data}
+          data={this.props.data}
           keyExtractor={(e, i) => (e.id).toString()}
           renderItem={(item, index) => (
             <TouchableOpacity onPress={() => this.openNotif(item)} >
@@ -159,6 +196,6 @@ class Enotification extends Component {
       </View>
     );
   }
-
 }
-module.exports = Enotification;
+module.exports = connect(Enotification.mapStateToProps)(Enotification);
+
