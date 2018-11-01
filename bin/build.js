196018f906ebeb9f8336793fa9bc5c4668c60edb
Danang Widiantoro <danang@fisip.net>
2018-11-01 13:19:15 +0700

    0.0.50

--- bin/build.js
+++ bin/build.js
@@ -3,17 +3,23 @@
 const { spawn } = require('child_process');
 const fs = require('fs');
 
-const path = "../../package.json"
-const pathScript = "../react-native-scripts/build/bin/react-native-scripts.js"
-if (fs.existsSync(path))
+const DIR = "../../"
+const packjson = DIR+"package.json"
+const appjson = DIR+"app.json"
+const appjs = DIR+"App.js"
+const pathScript = DIR+"node_modules/react-native-scripts/build/bin/react-native-scripts.js"
+if (fs.existsSync(packjson))
 {
-	var $package = JSON.parse(fs.readFileSync(path, 'utf8'));
+	var txt = fs.readFileSync(packjson, 'utf8');
+	var $package = JSON.parse(txt);
+	var rewrite = false;
+	var args = process.argv.slice(2);
+
 
 	/* ADD SCRIPTS.PRESTART AND SCRIPTS.POSTSTOP */
 	if (fs.existsSync(pathScript))
 	{
 		var $script = fs.readFileSync(pathScript, 'utf8');
-		var args = process.argv.slice(2);
 
 		/* Update react-native-scripts */
 		var code1    = "// the command is valid"
@@ -21,36 +27,87 @@ if (fs.existsSync(path))
 		var match    = $script.match(new RegExp(code1+"(\s{0,}\n\s{0,}[^\n]+)\n")); // mengambil 1 baris setelah code1
 		var $script2 = $script;
 		var espCode  = false;
-		if (match) {
-			if (args[0] == "install") {
+		if (match)
+		{
+			if (args[0] == "install")
+			{
 				var reg = new RegExp(/(\[.*?\),)/g) // mengambil argument ke 2 dari .sync()
 				var tag = match[1].match(reg)
-				if (tag) {
-					if (!tag[0].match(/esoftplay/)) {
+				if (tag)
+				{
+					if (!tag[0].match(/esoftplay/))
+					{
 						espCode = match[1].replace(reg, "['./node_modules/.bin/esoftplay', script],") // ganti argument ke 2 menjadi milik esoftplay
 						code2 = code1+espCode;
 						$script2 = $script.replace(code1, code2) // masukkan code ke dalam script setelah argument ke 2 di ganti ke esoftplay
 					}
 				}
 			}else
-			if (args[0] == "uninstall") {
-				if (match[1].match(/esoftplay/)) {
+			if (args[0] == "uninstall")
+			{
+				if (match[1].match(/esoftplay/))
+				{
 					$script2 = $script.replace(match[1], code2) // hapus script dari esoftplay
 				}
 			}
 		}
-		if ($script2!=$script) {
+		if ($script2!=$script)
+		{
 			fs.writeFile(pathScript, $script2, (err) => {
 			  if (err) throw err;
 			  console.log('react-native-scripts has been updated!!');
 			});
+		}
+	}else{
+		/* Update package.json for latest expo */
+		rewrite = false
+		if (!$package.hasOwnProperty("scripts"))
+		{
+			rewrite = true
+			$package.scripts = {
+				"start": "esp start && expo start",
+		    "android": "expo start --android",
+		    "ios": "expo start --ios",
+		    "eject": "expo eject"
+				};
+		}
+		if (args[0] == "install")
+		{
+			if (!$package.scripts.hasOwnProperty("start"))
+			{
+				rewrite = true;
+				$package.scripts.start = "esp start && expo start"
+			}else{
+				if (!$package.scripts.start.match(/esp start/))
+				{
+					rewrite = true
+					$package.scripts.start = "esp start && " + $package.scripts.start
+				}
+			}
+		}else
+		if (args[0] == "uninstall")
+		{
+			if ($package.scripts.start.match(/esp start/))
+			{
+				rewrite = true
+				$package.scripts.start = $package.scripts.start.replace(/esp start(\s+&&\s+)/ig, "");
+			}
+		}
+		if (rewrite)
+		{
+			console.log("Please change scripts.start in package.json into '"+$package.scripts.start+"'")
+			// spawn('node', ['./packager.js', args[0], packjson], { stdio: 'inherit' })
+			// fs.writeFile(packjson, JSON.stringify($package, null, 2), (err) => {
+			//   if (err) throw err;
+			//   console.log('package.json has been updated');
+			// });
 		}
 	}
 
 	/* Create ESP command line */
 	if (args[0] == "install")
 	{
-		spawn('esp', 'start', { stdio: 'inherit' })
+		spawn('esp', ['start'], { stdio: 'inherit' })
 			.on('exit', function(code) {
 				console.log(code);
 			})
@@ -72,9 +129,8 @@ if (fs.existsSync(path))
 	/* Update app.json */
 	if (args[0] == "install")
 	{
-		const appjson = "../../app.json"
 		var $app = JSON.parse(fs.readFileSync(appjson, 'utf8'));
-		var rewrite = false;
+		rewrite = false;
 		if (!$package.hasOwnProperty("name"))
 		{
 			$package.name = "esoftplay"
@@ -144,12 +200,12 @@ export default class App extends React.Component {\n\
       </Provider>\n\
   }\n\
 }";
-			fs.writeFile("../../App.js", AppJS, (err) => {
+			fs.writeFile(appjs, AppJS, (err) => {
 			  if (err) throw err;
 			  console.log('App.js has been updated');
 			});
 		}
 	}
 }else{
-	console.log(path+" not found!!")
+	console.log(packjson+" not found!!")
 }
---
+++ bin/packager.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+const { spawn } = require('child_process');
+const fs = require('fs');
+
+
+/* MENAMBAHKAN scrips.start di package.json */
+var args = process.argv.slice(2);
+const path = args[1] || "../../package.json"
+
+/* Update package.json for latest expo */
+if (fs.existsSync(path))
+{
+	var txt = fs.readFileSync(path, 'utf8');
+	var $package = JSON.parse(txt);
+	var rewrite = false;
+	if (!$package.hasOwnProperty("scripts"))
+	{
+		rewrite = true
+		$package.scripts = {
+			"start": "esp start && expo start",
+	    "android": "expo start --android",
+	    "ios": "expo start --ios",
+	    "eject": "expo eject"
+			};
+	}
+	if (args[0] == "install")
+	{
+		if (!$package.scripts.hasOwnProperty("start"))
+		{
+			rewrite = true;
+			$package.scripts.start = "esp start && expo start"
+		}else{
+			if (!$package.scripts.start.match(/esp start/))
+			{
+				rewrite = true
+				$package.scripts.start = "esp start && " + $package.scripts.start
+			}
+		}
+	}else
+	if (args[0] == "uninstall")
+	{
+		if ($package.scripts.start.match(/esp start/))
+		{
+			rewrite = true
+			$package.scripts.start = $package.scripts.start.replace(/esp start(\s+&&\s+)/ig, "");
+		}
+	}
+	if (rewrite)
+	{
+		fs.writeFile(path, JSON.stringify($package, null, 2), (err) => {
+		  if (err) throw err;
+		  console.log('package.json has been updated');
+		});
+	}
+
+}
